22234866ab0be7e9a83c2189526bba439947be0d
Abhishek Verma <abhishek.verma@fisdom.com>
2023-07-05 12:54:35 +0530

    Add : Code generator with files and templates

---
+++ plopfile.js
@@ -0,0 +1,49 @@
+module.exports = function (plop) {
+    plop.setGenerator("component", {
+        description: "Create a component",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is this component's name?",
+            },
+            {
+                type: "input",
+                name: "element",
+                message: "HTML element (div is default)",
+                default: "div",
+            },
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+                templateFile: "templates/Component.tsx.hbs",
+            },
+        ],
+    });
+
+    plop.setGenerator("featureStructure",{
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is this new feature's name?",
+            }
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "src/screens/{{pascalCase name}}/index.js",
+            },
+            {
+                type: "add",
+                path: "src/containers/{{pascalCase name}}/index.js",
+            },
+            {
+                type: "add",
+                path: "src/service/{{pascalCase name}}/index.js",
+            },
+        ],
+    })
+};
