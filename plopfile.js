2fb25515b4c0e6861df21892ab69cde1fb69d9d2
Abhishek Verma <abhishek.verma@fisdom.com>
2023-07-05 13:07:49 +0530

    Add : Description

--- plopfile.js
+++ plopfile.js
@@ -24,6 +24,7 @@ module.exports = function (plop) {
     });
 
     plop.setGenerator("featureStructure",{
+        description: "Create Feature folders with index.js",
         prompts: [
             {
                 type: "input",
